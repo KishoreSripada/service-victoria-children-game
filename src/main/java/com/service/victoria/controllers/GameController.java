b4ba8b846927ad4bb54c56d5dd999a4adf113868
Kishore Sripada <kishore.sripada@sensis.com.au>
2019-12-21 18:04:05 +1100

    Added LOGGER and base service class

--- src/main/java/com/service/victoria/controllers/GameController.java
+++ src/main/java/com/service/victoria/controllers/GameController.java
@@ -1,18 +1,27 @@
 package com.service.victoria.controllers;
 
+import com.service.victoria.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 public class GameController {
 
+    @Autowired
+    private GameService gameService;
+
+    Logger logger = Logger.getLogger(GameController.class.getName());
     @RequestMapping("/game")
     public int game(@RequestParam(value="noOfChildren", defaultValue="5") Integer noOfChildren,
-                                   @RequestParam(value="counter", defaultValue="6") String counter) {
-        System.out.println("noOfChildren is " + noOfChildren);
-        System.out.println("counter is " + counter);
-        Integer winnerChild = 2;
+                                   @RequestParam(value="counter", defaultValue="6") Integer counter) {
+        logger.info("noOfChildren is " + noOfChildren);
+        logger.info("counter is " + counter);
+        Integer winnerChild = gameService.playGame(noOfChildren, counter);
         return winnerChild;
     }
 }
---
+++ src/main/java/com/service/victoria/service/GameService.java
@@ -0,0 +1,11 @@
+package com.service.victoria.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+    public static Integer playGame(int noOfChildren, int counter) {
+        return 10;
+    }
+}

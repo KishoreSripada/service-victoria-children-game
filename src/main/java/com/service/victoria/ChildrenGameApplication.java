be80efbd40de4fdcbe781739a76455e1644fc6ad
Kishore Sripada <kishore.sripada@sensis.com.au>
2019-12-21 17:27:31 +1100

    Adding base structure of spring boot

---
+++ src/main/java/com/service/victoria/ChildrenGameApplication.java
@@ -0,0 +1,13 @@
+package com.service.victoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/victoria/ChildrenGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.victoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
